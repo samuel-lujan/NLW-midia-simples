9cd84f68b53e9bb51148c519b65474e13df7b075
Samuel Lujan <samuel_lujan@hotmail.com>
2021-03-02 09:08:31 -0300

    Adicionando configurações e importações do css, para a criação da primeira página de teste

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,14 @@
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className="row">
+        <div className="col">
+            <h1>Teste</h1>
+        </div>
+        <div className="col">
+          <h2>Teste2</h2>
+        </div>
+    </div>
+  )
+}

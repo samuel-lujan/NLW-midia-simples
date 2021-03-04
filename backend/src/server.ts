68dedd8ff8413efa50ba6760f1c70fd492704056
Samuel Lujan <samuel_lujan@hotmail.com>
2021-03-04 09:08:24 -0300

    Adicionando morgan, para verficar requisoes feia, e adicionando cors para abrir o backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata';
 import "../database"
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
+app.use(morgan('dev'));
 app.use(router);
 
 app.listen(3333, () => 

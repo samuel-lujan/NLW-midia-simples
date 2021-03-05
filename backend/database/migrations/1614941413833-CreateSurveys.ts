0ff50aef489613ef45330c71f2577e691807e886
Samuel Lujan <samuel_lujan@hotmail.com>
2021-03-05 08:00:27 -0300

    Criando a migration de perguntas

---
+++ backend/database/migrations/1614941413833-CreateSurveys.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveys1614941413833 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'surveys',
+                columns: 
+                [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: "now()"
+                    }
+                ]
+            }) 
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('surveys');
+    }
+
+}

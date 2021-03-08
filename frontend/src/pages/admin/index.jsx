63c3c90119d9fe5aaefac592939b7f950656a773
Samuel Lujan <samuel_lujan@hotmail.com>
2021-03-08 09:05:17 -0300

    Adicionando link para página de perguntas

--- frontend/src/pages/admin/index.jsx
+++ frontend/src/pages/admin/index.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import styles from '../../styles/pages/Admin.module.css'
 import cx from 'classnames';
+import Link from 'next/link';
 
 export default function Admin(){
     return (
         <div className="container d-flex align-items-center justify-content-center h-100">
             <div className={cx(styles.painel, 'd-flex align-items-center w-100')}>
                 <div className="d-flex justify-content-around w-100">
-                    <button type="button" className="btn btn-primary btn-lg ">
-                        Cadastro de Perguntas
-                    </button>
-                    <button type="button" className="btn btn-primary btn-lg">
-                        Botão Grande
-                        </button>
+                    <Link href="/admin/perguntas">
+                        <a className="btn btn-primary btn-lg">Cadastro de Perguntas</a>
+                    </Link>
                 </div>
             </div>
         </div>

c41960c2d8567dcfca126d5882011c69d16a78a9
Samuel Lujan <samuel_lujan@hotmail.com>
2021-03-10 08:55:45 -0300

    Alterando modo de conexão do server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,4 @@
-import 'reflect-metadata';
-import "../database"
-import express from 'express';
-import { router } from './routes';
-import cors from 'cors';
-import morgan from 'morgan';
-
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
-app.use(router);
+import { app } from "./app";
 
 app.listen(3333, () => 
     console.log('foi')

b2cbdc5a09d06f34dc8175ce867bbcd541a3abbe
Samuel Lujan <samuel_lujan@hotmail.com>
2021-03-12 08:57:00 -0300

    Criando rotas para consulta de pergunta com usuário

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,19 +2,24 @@ import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController'
 import { SendMailController } from './controllers/SendMailController';
+import { SurveysUsersController } from './controllers/SurveysUsersController';
 
 const router = Router();
 
 const userController        = new UserController();
 const surveysController     = new SurveysController();
 const sendMailController    = new SendMailController();
+const surveysUsersController = new SurveysUsersController(); 
 
 router.post('/users', userController.create);
 
 
-router.get('/surveys', surveysController.show);
+router.get('/surveys', surveysController.index);
 router.post('/surveys', surveysController.create);
 
 router.post('/sendMail', sendMailController.execute);
+
+router.get('/surveys/users/:idSurveyUser', surveysUsersController.show);
+router.put('/surveys/users/:idSurveyUser', surveysUsersController.update);
 
 export { router }
